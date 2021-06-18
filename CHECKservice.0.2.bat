1fbd571c55cfc7ad00c16df261f2d2e899ed64c5
Jacobo <jacobnorth1700@gmail.com>
2021-06-18 10:19:46 -0500

    Correcting and fixing code with FUNCTION

--- CHECKservice.0.2.bat
+++ CHECKservice.0.2.bat
@@ -1,59 +1,46 @@
 @ECHO OFF
 
-set /p id="Enter Service: "
+		:checkSERVICE
+			SET /p ID="Enter Service here: "
+			SET checkSERVICENAME=%ID%
 
-SET nameServer=%id%
-	SC QUERYEX FIND "ALL" | FIND "STATE" | FIND /v "RUNNING" > NUL && (
-		ECHO %nameServer% is not running > NUL
-		
-		ECHO starting %nameServer% > NUL
-		net start "%nameServer%" > NUL || (
-			ECHO "%nameServer%" will not start
-			TIMEOUT /t 1 > NUL
-			exit /b 1 
+			SC QUERYEX "%checkSERVICENAME%" | FIND "STATE" | FIND /v "RUNNING" > NUL && (
+				ECHO %checkSERVICENAME% is not running
+				ECHO starting %checkSERVICENAME%
+				net start "%checkSERICENAME%" > NUL || (
+					ECHO "%checkSERVICENAME%" will not start
+					TIMEOUT /t 1
+					PAUSE
+				)
+				ECHO %checkSERVICENAME% is started.
 			)
-		ECHO %nameServer% is started > NUL
-		)
-		IF %ERRORLEVEL% NEQ 0 Echo %nameServer% is running > NUL
-		TIMEOUT /t 2 > NUL
+			IF %ERRORLEVEL% NEQ 0 ECHO %checkSERVICENAME% is running
+			TIMEOUT /t 2
 
-	SC QUERYEX "%nameServer%" | FIND "STATE" | FIND /v "RUNNING" > NUL && (
-		ECHO %nameServer% is not running > NUL || (
-			TIMEOUT /t 1 > NUL
-			ECHO "%nameServer%" will not start
-			TIMEOUT /t 2 > NUL
-			PAUSE
+			SC QUERYEX "%checkSERVICENAME%" | FIND "STATE" | FIND /v "RUNNING" > NUL && (
+				ECHO %checkSERVICENAME% is not running || (
+					TIMEOUT /t 2
+					ECHO "%checkSERVICENAME%" will not start
+					TIMEOUT /t 2
+					PAUSE
+				)
 			)
-		)
-		IF %ERRORLEVEL% NEQ 0 Echo %nameServer% is running fine > NUL
-		TIMEOUT /t 1 > NUL
-	
-	SC QUERYEX "%nameServer%" | FIND "STATE" | FIND /v "STOPPED" > NUL && (
-		net stop "%nameServer%" > NUL & (
-			ECHO %nameServer% is stopped > NUL
-			TIMEOUT /t 1 > NUL
+			IF %ERRORLEVEL% NEQ 0 ECHO %checkSERVICENAME% is running fine
+			TIMEOUT /t 2
+
+			SC QUERYEX "%checkSERVICENAME%" | FIND "STATE" | FIND /v "STOPPED" > NUL && (
+				net stop "%checkSERVICENAME%" > NUL & (
+					ECHO %checkSERVICENAME% is stopped
+					TIMEOUT /t 1
+				)
 			)
-		)
-	
-	SC QUERYEX "%nameServer%" | FIND "STATE" | FIND /v "STOPPED" < NUL && (
-		ECHO %nameServer% will not stop > NUL
-		)
-		ECHO verified that %nameServer% is not running > NUL
-		PAUSE
-		
-		
 
+			SC QUERYEX "%checkSERVICENAME%" | FIND "STATE" | FIND /v "STOPPED" < NUL && (
+				ECHO %checkSERVICENAME% will not stop
+			)
 
-			
-		
+			ECHO verified that %checkSERVICENAME% is not running
+			TIMEOUT /t 4
+			EXIT /b 0
 
-
-	
-
-	
-	
-	
-	
-	
-		
-	
+		CALL :checkSERVICE
